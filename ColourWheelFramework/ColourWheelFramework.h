3fccde1e59bde3720011ae1b145bdf7b5ee9348b
Julian Porter <julian@jpembedded.solutions>
2019-03-31 20:58:43 +0100

    Improving cocoapod

---
+++ ColourWheelFramework/ColourWheelFramework.h
@@ -0,0 +1,19 @@
+//
+//  ColourWheelFramework.h
+//  ColourWheelFramework
+//
+//  Created by Julian Porter on 31/03/2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ColourWheelFramework.
+FOUNDATION_EXPORT double ColourWheelFrameworkVersionNumber;
+
+//! Project version string for ColourWheelFramework.
+FOUNDATION_EXPORT const unsigned char ColourWheelFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColourWheelFramework/PublicHeader.h>
+
+
